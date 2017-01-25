e64cd032c53eeb2a01be0c50d1478c82507259d8
anonymous0010 <ctoxtli@gmail.com>
2017-01-25 15:26:16 -0500

    New changes

---
+++ commits.py
@@ -0,0 +1,32 @@
+from random import randint
+from datetime import date, timedelta as td
+
+d1 = date(2010, 1, 1)
+d2 = date(2011, 12, 31)
+
+delta = d2 - d1
+
+print("""
+#!/bin/bash
+REPO=deprecated
+git init testing
+cd testing
+touch README.md
+git add README.md
+touch tox
+git add tox
+""")
+
+for i in range(delta.days + 1):
+    current = str(d1 + td(days=i))
+    for j in range(randint(1,10)):
+    	command = 'GIT_AUTHOR_DATE=' + current + 'T12:00:00 GIT_COMMITTER_DATE=' + current + 'T12:00:00 git commit --allow-empty -m "tox" > /dev/null'
+    	print(command)
+
+print("""
+
+git remote add origin git@github.com:toxtli/testing.git
+git pull origin master
+git push -u origin master
+
+""")
